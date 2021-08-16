5059b28b99020684d7efdc5dd99859514998c2bd
chams-az <chams.azouz@mdpi.com>
2021-08-16 14:30:47 +0200

    Plot modif bert

--- main.py
+++ main.py
@@ -2,37 +2,33 @@ import os
 
 from dospacy import trainSpacyModel
 from dospacy import testSpacyModel
-# from dobilstm import trainBiLSTMModel
+from dospacy import evaluateSpacy
+from dobilstm import trainBiLSTMModel
 from datetime import datetime
 from dobert import trainBERT, evaluation
 
-
 def train_model(model, modelFile):
     LABEL = ['LOCATION', 'CONTENT', 'TRIGGER', 'MODAL', 'ACTION']
-    # LABEL = ['LOCATION', 'TRIGGER', 'MODAL', 'ACTION']
+    #LABEL = ['LOCATION', 'TRIGGER', 'MODAL', 'ACTION']
 
     ROOT_DIR = os.path.dirname(os.path.abspath('data.json'))
-    path_csv = os.path.join(ROOT_DIR, 'data_full_test.csv')
+    path_train_data = os.path.join(ROOT_DIR, 'data_train_small.json')
 
-    dropout = 1e-4
-    nIter = 10
+    dropout = 1e-5
+    nIter   = 100
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Start time = ", current_time)
 
     if model == str(1):
-        nlp = trainSpacyModel(path_csv, LABEL, dropout, nIter)
-        # Save the trained Model
-        nlp.to_disk(os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + modelFile)
+        nlp = trainSpacyModel(path_train_data, LABEL, dropout, nIter)
     elif model == str(3):
-        nlp = trainBERT(ROOT_DIR+"/"+modelFile + ".json")
+        nlp = trainBERT(ROOT_DIR + "/" + modelFile + ".json")
+        exit()
     else:
         if model == str(2):
-            print("no")
-            # nlp = trainBiLSTMModel(path_csv, LABEL, dropout, nIter)
-            # Save the trained Model
-            #nlp.to_disk(modelFile)
+            nlp = trainBiLSTMModel(path_train_data, LABEL, dropout, nIter)
         else:
             exit("Wrong model selection")
 
@@ -40,30 +36,57 @@ def train_model(model, modelFile):
     current_time = now.strftime("%H:%M:%S")
     print("End time = ", current_time)
 
+    # Save the trained Model
+    nlp.to_disk(os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + modelFile)
 
 
-def test_model_manually(model_name):
+def test_model_manually(model_path):
     number_of_testing_examples = input("Enter the number of testing examples: ")
-    testSpacyModel(model_name, number_of_testing_examples)
+    testSpacyModel(model_path, number_of_testing_examples)
 
-
-def test_model(data_name, model_name):
-    evaluation(data_name,model_name)
+def test_model_dataset(model_path,modelname):
+    choice=input("1:berteval 0:else")
+    if choice=="1":
+        evaluation(data_name,model_name)
+    else:
+        model_path= os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
+        LABEL = ['LOCATION', 'CONTENT', 'TRIGGER', 'MODAL', 'ACTION']
+        ROOT_DIR = os.path.dirname(os.path.abspath('data.json'))
+        path_test_data = os.path.join(ROOT_DIR, 'data_test_full.json')
+        results = evaluateSpacy(model_path, path_test_data, LABEL)
+        print("Entity\t\tPrecision\tRecall\tF-score")
+        for result in results['ents_per_type']:
+            if(result == "LOCATION"):
+                print(
+                    "{:}\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                        format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'],
+                               results['ents_per_type'][result]['f'])
+                )
+            else:
+                print(
+                    "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                        format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'], results['ents_per_type'][result]['f'])
+                )
+        print()
+        print(
+            "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                format("TOTAL", results['ents_p'], results['ents_r'], results['ents_f'])
+        )
 
 
 if __name__ == '__main__':
     action_type = input("Action type: (1. Train; 2. Dataset Test; 3. Manual Test;): ")
     if action_type == str(1):
-        model_type = input("Model (1. spaCy; 2. Bi-LSTM; 3. BERT): ")
+        model = input("Model (1. spaCy; 2. Bi-LSTM; 3. BERT): ")
         modelFile = input("Enter the Model name to save: ")
-        train_model(model_type, modelFile)
+        train_model(model, modelFile)
     else:
         if action_type == str(2):
             data_name = input("datafile name to test: ")
-            model_name = input("modelfile name to test: ")
-            # test_model_dataset(model_name)
-            test_model( data_name , model_name)
+            model_name = input("Model name to test: ")
+            test_model_dataset(data_name , model_name)
 
         else:
             model_name = input("Model name to test: ")
-            test_model_manually(model_name)
+            model_path = os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
+            test_model_manually(model_path)
