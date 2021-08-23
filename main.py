ad566831b22b8cc0f03dbe284bb8a7c61c0a8335
Milos Cuculovic <cuculovic@mdpi.com>
2021-08-23 10:02:56 +0200

    Small modif on the main

--- main.py
+++ main.py
@@ -50,34 +50,35 @@ def test_model_manually(model_path):
     number_of_testing_examples = input("Enter the number of testing examples: ")
     testSpacyModel(model_path, number_of_testing_examples)
 
-def test_model_dataset(model_path, modelname):
+def test_model_dataset(model_name):
     choice = input("1:berteval 0:else")
     if choice == "1":
-        evaluation(data_name, model_name)
+        valid_path = ''
+        evaluation(valid_path, model_name)
     else:
-        model_path = os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
-    LABEL = ['LOCATION', 'CONTENT', 'TRIGGER', 'MODAL', 'ACTION']
-    ROOT_DIR = os.path.dirname(os.path.abspath('data.json'))
-    path_test_data = os.path.join(ROOT_DIR, 'data_test_full.json')
-    results = evaluateSpacy(model_path, path_test_data, LABEL)
-    print("Entity\t\tPrecision\tRecall\tF-score")
-    for result in results['ents_per_type']:
-        if(result == "LOCATION"):
-            print(
-                "{:}\t{:0.4f}\t{:0.4f}\t{:0.4f}".
-                    format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'],
-                           results['ents_per_type'][result]['f'])
-            )
-        else:
-            print(
-                "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
-                    format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'], results['ents_per_type'][result]['f'])
-            )
-    print()
-    print(
-        "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
-            format("TOTAL", results['ents_p'], results['ents_r'], results['ents_f'])
-    )
+        #model_path = os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
+        LABEL = ['LOCATION', 'CONTENT', 'TRIGGER', 'MODAL', 'ACTION']
+        ROOT_DIR = os.path.dirname(os.path.abspath('data.json'))
+        path_test_data = os.path.join(ROOT_DIR, 'data_test_full.json')
+        results = evaluateSpacy(model_name, path_test_data, LABEL)
+        print("Entity\t\tPrecision\tRecall\tF-score")
+        for result in results['ents_per_type']:
+            if(result == "LOCATION"):
+                print(
+                    "{:}\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                        format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'],
+                               results['ents_per_type'][result]['f'])
+                )
+            else:
+                print(
+                    "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                        format(result, results['ents_per_type'][result]['p'], results['ents_per_type'][result]['r'], results['ents_per_type'][result]['f'])
+                )
+        print()
+        print(
+            "{:}\t\t{:0.4f}\t{:0.4f}\t{:0.4f}".
+                format("TOTAL", results['ents_p'], results['ents_r'], results['ents_f'])
+        )
 
 
 if __name__ == '__main__':
@@ -92,7 +93,7 @@ if __name__ == '__main__':
             model_path = os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
             test_model_dataset(model_path)
         else:
-            if action_type == str(2):
+            if action_type == str(3):
                 model_name = input("Model name to test: ")
                 model_path = os.path.dirname(os.path.abspath(__file__)) + '/trained_models/' + model_name
                 test_model_manually(model_path)
